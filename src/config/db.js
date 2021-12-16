04ef906eb82105738ae3436373933f4b80475ca5
dracero <diego.racero@gmail.com>
2021-12-16 13:49:25 -0300

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require ('mongoose')
+
+const connection = mongoose.connect(process.env.MONGO_ATLAS_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', ()=> {
+    console.log('[Mongoose] - connected in:', process.env.MONGO_ATLAS_URL)
+})
+
+mongoose.connection.on('error', (err)=> {
+    console.log('[Mongoose] - error:', err)
+})
+
+module.exports = connection
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const cluster = require ('cluster')
+const numCPUs = require ('os').cpus().length
+
+if (process.argv[5] == 'CLUSTER'){
+      if(cluster.isMaster){
+      console.log(numCPUs)
+      console.log(`PID MASTER ${process.pid}`)
+  
+      for( let i = 0; i < numCPUs; i++ ){
+        cluster.fork()
+      }
+      cluster.on('exit',worker => {
+        console.log(`Worker PID: ${worker.process.pid} died ${new Date().toLocaleString()}`)
+        
+        cluster.fork()
+      })
+    }else {
+        require('./desafio18.js')
+    }
+  }else {
+    require('./desafio18.js')
+
+  }
