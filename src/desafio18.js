ac5bbddc8dc00f61382080c099765b663c8fb39e
dracero <diego.racero@gmail.com>
2022-01-30 20:43:53 -0300

    first commit

--- src/desafio18.js
+++ src/desafio18.js
@@ -1,28 +1,21 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express') 
 const handlebars = require('express-handlebars')  
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
 const MongoStore = require('connect-mongo')
 const passport = require('passport')
 const compression = require('compression')
-var cors = require('cors')
 
 const { logger, loggerError } = require('./logger/config')
 
 require('dotenv').config()
 
-
 const initListeners = require('./listeners')
 const app = express()
 app.use(compression())
-app.use(cors())
 
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-
-//-- Database Connection 
-require('./config/db')
 
 //-- Passport Config
 require('./config/passport')(passport)
@@ -30,7 +23,7 @@ require('./config/passport')(passport)
 //-- Session y coookies 
 app.use(session({
   store: MongoStore.create({ 
-    mongoUrl: `${process.env.MONGO_URL}`,
+    mongoUrl: `${process.env.MONGO_ATLAS_URL}`,
     ttl: 60 * 10 
   }),
   secret: 'secreto',
@@ -38,7 +31,9 @@ app.use(session({
   saveUninitialized: true,
 }))
 
-app.use(morgan('dev'))
+if(process.env.ENV == 'DEV') {
+  app.use(require('morgan')('dev'))
+}
 
 app.use(cookieParser())
 app.use(express.json())
@@ -47,7 +42,6 @@ app.use(express.urlencoded({ extended: true }))
 //--Passport Middlewares 
 app.use(passport.initialize())
 app.use(passport.session())
-
 
 //Public folder + Handlebars
 app.use(express.static(__dirname + '/public'))
@@ -66,11 +60,15 @@ app.use('/auth',require('./routes/auth'))
 //Rutas API
 app.use('/api/productos', require('./routes/api/productos'))
 app.use('/api/auth', require('./routes/api/auth'))
- 
+
+
+//Graphql
+app.use('/graphql', require('./graphql'))
+
 //Socket
 initListeners(io)
 
-const PORT = process.argv[2] ||  process.env.PORT || 8080 
+const PORT = process.env.PORT || 8080 
 
 
 const server = http.listen(PORT, () => {
